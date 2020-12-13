d1154c088343517675e197a8769baf8bcb7b4b19
Rodin van der Hart <rodin.hart@concentra.co.uk>
2020-12-13 19:36:08 +0000

    zip

--- core.clj
+++ core.clj
@@ -1,1 +1,13 @@
-(cons 2 (quote (3 5)))
+(def zip (fn (f xs ys)
+  (if xs
+    (cons
+      (f (car xs) (car ys))
+      (zip f (cdr xs) (cdr ys))
+    )
+    nil
+  )
+))
+
+(zip + (quote (1 2 3)) (quote (4 5 6)))
+
+nil
